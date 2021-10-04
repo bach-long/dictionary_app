8b3c39c5e43e105478d3b144c9c1cadfd25e4a35
bach-long <nguyenbach19122002@gmail.com>
2021-10-04 10:48:53 +0700

    add,delete,edit

---
+++ database_manage.java
@@ -0,0 +1,124 @@
+package dictionary;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.lang.String;
+import java.text.SimpleDateFormat;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.sql.Timestamp;
+
+public class database_manage {
+    static Connection c = null;
+    static Statement stmt = null;
+    static ResultSet rs = null;
+    static PreparedStatement pstsm = null;
+
+    public static void set_database() {
+        try {
+            c = DriverManager.getConnection("jdbc:sqlite:./src/main/resources/dict_avva.db");
+            c.setAutoCommit(true);
+            stmt = c.createStatement();
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() );
+            System.exit(0);
+        }
+    }
+
+    public static void close_database() throws SQLException {
+        if(rs == null) {}
+        else {
+            rs.close();
+        }
+        if(pstsm == null){}
+        else {
+            pstsm.close();
+        }
+        stmt.close();
+        c.close();
+    }
+
+    public static String search(String r) throws SQLException{
+        String s="";
+        rs = stmt.executeQuery(String.format("select * from av where word = '%s'", r.toLowerCase()));
+        while (rs.next()) {
+            String drc = rs.getString("html");
+            s = s + drc + "\n";
+
+        }
+        if(rs==null) {}
+        else {
+            rs.close();
+        }
+        return s;
+    }
+
+    public static String[] list_word() throws SQLException {
+        rs = stmt.executeQuery("select * from av");
+        Set<String> a = new LinkedHashSet<> ();
+        while(rs.next()) {
+            a.add(rs.getString("word"));
+        }
+        String[] result = new String [a.size()];
+        result = a.toArray(result);
+        rs.close();
+        return result;
+    }
+
+    public static void user_add (String word, String mean) throws SQLException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        pstsm = c.prepareStatement("INSERT INTO user (word, definition, time) VALUES (?,?,?)");
+        pstsm.setString(1,word);
+        pstsm.setString(2,mean);
+        pstsm.setString(3,sdf.format(timestamp));
+        pstsm.execute();
+        if (pstsm == null){}
+        else {
+            pstsm.close();
+        }
+    }
+
+    public static void make_change (String word, String mean) throws SQLException {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        Timestamp timestamp = new Timestamp(System.currentTimeMillis());
+        pstsm = c.prepareStatement("UPDATE user SET DEFINITION = ?, time = ? WHERE word = ?");
+        pstsm.setString(1,mean);
+        pstsm.setString(2, sdf.format(timestamp));
+        pstsm.setString(3,word);
+        pstsm.execute();
+        if (pstsm == null){}
+        else {
+            pstsm.close();
+        }
+    }
+
+    public static void user_delete (String s) throws SQLException {
+        pstsm = c.prepareStatement("DELETE FROM user WHERE word = ?");
+        pstsm.setString(1,s);
+        pstsm.execute();
+        if (pstsm == null){}
+        else {
+            pstsm.close();
+        }
+    }
+
+    public static String user_search(String r) throws SQLException {
+        String s="";
+            rs = stmt.executeQuery(String.format("select * from user where word = '%s'", r.toLowerCase()));
+            while (rs.next()) {
+                String drc = rs.getString("definition");
+                s = s + drc + "\n";
+
+            }
+        if(rs == null) {}
+        else
+        rs.close();
+        return s;
+    }
+}
+
